2e6748aab09b6b24c51d01446c97fa328b150716
Vitor Lucas <vitorluc.2013@gmail.com>
2020-10-01 12:16:12 -0300

    DTO

--- backend/src/main/java/com/vitorlucas/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/vitorlucas/dscatalog/services/CategoryService.java
@@ -1,13 +1,13 @@
 package com.vitorlucas.dscatalog.services;
 
 import java.util.List;
-
-
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.vitorlucas.dscatalog.dto.CategoryDTO;
 import com.vitorlucas.dscatalog.entities.Category;
 import com.vitorlucas.dscatalog.repositories.CategoryRepository;
 
@@ -18,7 +18,16 @@ public class CategoryService {
 	private CategoryRepository repositoy;
 	
 	@Transactional(readOnly = true)
-	public List<Category> findAll(){
-		return repositoy.findAll();
+	public List<CategoryDTO> findAll(){
+		List<Category> list = repositoy.findAll();
+		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+
+/*		List<CategoryDTO> listDTO = new ArrayList<>();
+		for (Category cat : list) {
+			listDTO.add(new CategoryDTO(cat));
+		}
+		return listDTO;
+*/
+		
 	}
 }
