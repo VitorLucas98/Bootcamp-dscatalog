687c1bf11ba5bbd6ce1af7d98895d8e6030ccdc4
Vitor Lucas <vitorluc.2013@gmail.com>
2020-10-01 11:33:14 -0300

    Database seeding

--- backend/src/main/java/com/vitorlucas/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/vitorlucas/dscatalog/services/CategoryService.java
@@ -2,8 +2,11 @@ package com.vitorlucas.dscatalog.services;
 
 import java.util.List;
 
+
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.vitorlucas.dscatalog.entities.Category;
 import com.vitorlucas.dscatalog.repositories.CategoryRepository;
@@ -14,6 +17,7 @@ public class CategoryService {
 	@Autowired
 	private CategoryRepository repositoy;
 	
+	@Transactional(readOnly = true)
 	public List<Category> findAll(){
 		return repositoy.findAll();
 	}
