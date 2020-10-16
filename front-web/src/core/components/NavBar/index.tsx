a2d7b3f46d482fc6c30b44182209beff401d71d8
Vitor Lucas <vitorluc.2013@gmail.com>
2020-10-15 22:23:54 -0300

    Finish catalog loaders

--- front-web/src/core/components/NavBar/index.tsx
+++ front-web/src/core/components/NavBar/index.tsx
@@ -13,7 +13,7 @@ const Navbar = () => {
             <div className="col-6 offset-2">
                 <ul className='main-menu'>
                     <li>
-                        <NavLink to='/' activeClassName='active' exact> HOME</NavLink>
+                        <NavLink to='/' activeClassName='active' exact>HOME</NavLink>
                     </li>
                     <li>
                         <NavLink to='/products' activeClassName='active'>CATÁLOGO</NavLink>
--- front-web/src/core/pages/Catalog/index.tsx
+++ front-web/src/core/pages/Catalog/index.tsx
@@ -2,27 +2,35 @@ import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import ProductCard from './components/ProductCard';
 import './styles.scss';
-import { makeRequest } from '../../utils/request';
-import { ProductsResponse } from '../../types/Product';
+import { makeRequest } from 'core/utils/request';
+import { ProductsResponse } from 'core/types/Product';
+import ProductCardLoader from './components/Loaders/ProductCardLoader';
 
 const Catalog = () => {
     const [productResponse, setProductsResponse] = useState<ProductsResponse>();
+    const [isLoading, setIsLoading] = useState(false);
 
     useEffect(() =>{
         const params ={
             page:0,
             linesPerPage: 12
         }
+        setIsLoading(true);
         makeRequest({url: '/products', params})
-        .then(response => setProductsResponse(response.data));
+        .then(response => setProductsResponse(response.data))
+        .finally(() =>{
+            setIsLoading(false)
+        })
     },[]);
     return (
         <div className='catalog-container'>
             <h1 className='catalog-title'>Catalógo de produtos</h1>
             <div className="catalog-products">
-                {productResponse?.content.map(product =>(
-                <Link to={`/products/${product.id}`} key ={product.id} ><ProductCard product ={product}/></Link>
-                ))}
+               {isLoading ? <ProductCardLoader/> : (
+                    productResponse?.content.map(product =>(
+                    <Link to={`/products/${product.id}`} key ={product.id} ><ProductCard product ={product}/></Link>
+                    ))
+               )}
             </div>
         </div>
     );
