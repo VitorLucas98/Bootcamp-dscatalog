cb64f2c9c89d5bbd9b3866d4327503742e9e8091
Vitor Lucas <vitorluc.2013@gmail.com>
2020-11-16 21:34:26 -0300

    rebuilding the project

---
+++ backend/src/main/java/com/vitorlucas/dscatalog/dto/UserInsertDTO.java
@@ -0,0 +1,29 @@
+package com.vitorlucas.dscatalog.dto;
+
+import com.vitorlucas.dscatalog.services.validation.UserInsertValid;
+
+@UserInsertValid
+public class UserInsertDTO extends UserDTO{
+
+	private static final long serialVersionUID = 1L;
+	
+	private String password;
+	
+	public UserInsertDTO() {
+		super();
+	}
+
+	public UserInsertDTO(String password) {
+		super();
+		this.password = password;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+}
---
+++ backend/src/main/java/com/vitorlucas/dscatalog/dto/UserUpdateDTO.java
@@ -0,0 +1,10 @@
+package com.vitorlucas.dscatalog.dto;
+
+import com.vitorlucas.dscatalog.services.validation.UserUpdateValid;
+
+@UserUpdateValid
+public class UserUpdateDTO extends UserDTO{
+
+	private static final long serialVersionUID = 1L;
+		
+}
