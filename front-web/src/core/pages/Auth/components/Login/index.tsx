229cfef73ec48067091c5d72f3c4ad20ab8bfa3f
Vitor Lucas <vitorluc.2013@gmail.com>
2020-11-17 21:51:51 -0300

    Create auth module

--- front-web/src/core/pages/Auth/components/Login/index.tsx
+++ front-web/src/core/pages/Auth/components/Login/index.tsx
@@ -1,12 +1,43 @@
 import React from 'react';
 import AuthCard from '../Card';
 import './styles.scss';
+import { useForm } from 'react-hook-form';
+import { Link } from 'react-router-dom';
+import ButtonIcon from 'core/components/ButtonIcon';
 
-const Login = () =>{
+type FormData = {
+    email: string;
+    password:string;
+}
+
+const Login = () => {
+    const { register, handleSubmit} = useForm<FormData>();
+    const onSubmit = (data:FormData) => {
+        console.log(data);
+      };
     return (
         <AuthCard title='login'>
-            <form className='login-form'>
-                <h1>Formulario de login</h1>
+            <form className='login-form' onSubmit={handleSubmit(onSubmit)}>
+                <input type="email"
+                    className='input-base form-control margin-bottom-30'
+                    placeholder='Email'
+                    name="email" ref={register}  />
+                <input type="password"
+                    className='input-base form-control'
+                    placeholder='Senha' 
+                    name="password" ref={register} />
+                <Link to='/admin/auth/recover' className='login-link-recover'>
+                    Esqueci a senha ?
+                </Link>
+                <div className='login-submit'>
+                    <ButtonIcon text='logar' />
+                </div>
+                <div className='text-center'>
+                    <span className='not-registered'>Não tem Cadastro?</span>
+                    <Link to='/admin/auth/register' className='login-link-register'>
+                        CADASTRAR
+                    </Link>
+                </div>
             </form>
         </AuthCard>
     );
