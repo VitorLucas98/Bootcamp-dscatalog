bc3cf0284c0b0409bc0aef1136ac5acf32ac1cb8
Vitor Lucas <vitorluc.2013@gmail.com>
2020-12-06 19:49:49 -0300

    Integrating the ADMIN product listing with the backend - Completed

--- front-web/src/core/pages/Admin/components/Products/List/index.tsx
+++ front-web/src/core/pages/Admin/components/Products/List/index.tsx
@@ -1,21 +1,47 @@
-import React from 'react';
+import Pagination from 'core/components/Pagination';
+import { ProductsResponse } from 'core/types/Product';
+import { makeRequest } from 'core/utils/request';
+import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import Card from '../Card';
 
-const List = () =>{
+const List = () => {
+    const [productResponse, setProductsResponse] = useState<ProductsResponse>();
+    const [isLoading, setIsLoading] = useState(false);
+    const [activePage, setActivePage] = useState(0);
     const history = useHistory();
-    const handleCreate = () =>{
+
+    useEffect(() => {
+        const params = {
+            page: activePage,
+            linesPerPage: 5
+        }
+        setIsLoading(true);
+        makeRequest({ url: '/products', params })
+            .then(response => setProductsResponse(response.data))
+            .finally(() => {
+                setIsLoading(false)
+            })
+    }, [activePage]);
+
+    const handleCreate = () => {
         history.push('/admin/products/create')
     }
     return (
-        <div className ='admin-products-list'>
+        <div className='admin-products-list'>
             <button className='btn btn-primary btn-lg' onClick={handleCreate}>
                 ADICIONAR
             </button>
             <div className="admin-list-container">
-                <Card/>
-                <Card/>
-                <Card/>
+                {productResponse?.content.map(product => (
+                    <Card product={product} key={product.id} />
+                ))}
+                {productResponse &&
+                    <Pagination
+                        totalPages={productResponse.totalPages}
+                        activePage={activePage}
+                        onChange={page => setActivePage(page)}
+                    />}
             </div>
         </div>
     );
