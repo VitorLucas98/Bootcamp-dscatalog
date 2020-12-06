d0f3a8da4c4220ca5b07d3c12448ccc240bd7733
Vitor Lucas <vitorluc.2013@gmail.com>
2020-12-06 15:41:05 -0300

    creating product listing layout for the admin - part 2

--- front-web/src/core/pages/Admin/components/Products/Card/index.tsx
+++ front-web/src/core/pages/Admin/components/Products/Card/index.tsx
@@ -1,21 +1,32 @@
+import ProductPrice from 'core/components/ProductPrice';
 import React from 'react';
 import './styles.scss'
 
 
-const Card = () =>{
-    return(
+const Card = () => {
+    return (
         <div className='card-base product-card-admin '>
             <div className="row">
                 <div className='col-2 text-center border-right py-3'>
-                    <img src="https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/3-big.jpg" 
-                    alt="Produto teste" 
-                    className='product-card-image-admin'/>
+                    <img src="https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/3-big.jpg"
+                        alt="Produto teste"
+                        className='product-card-image-admin' />
                 </div>
                 <div className='col-7 py-3'>
                     <h3 className='product-card-name-admin'>Computador Desktop - Intel Core i7</h3>
+                    <ProductPrice price={4000} />
                 </div>
-                <div className='col-3 py-3'>
-                    <h1>ações</h1>
+                <div className='col-3 pt-3 pr-5'>
+                    <button
+                        type="button"
+                        className="btn btn-outline-secondary btn-block border-radius-10 mb-3 btn-edit">
+                        EDITAR
+                    </button>
+                    <button
+                        type="button"
+                        className="btn btn-outline-danger btn-block border-radius-10">
+                        EXCLUIR
+                    </button>
                 </div>
             </div>
         </div>
