872694feabbbca4f8e39456135568cf81119beac
Vitor Lucas <vitorluc.2013@gmail.com>
2021-03-18 23:07:41 -0300

    Fix Product Filters

--- front-web/src/core/components/ProductFilters/index.tsx
+++ front-web/src/core/components/ProductFilters/index.tsx
@@ -5,19 +5,25 @@ import Select from 'react-select';
 import { makeRequest } from 'core/utils/request';
 import { Category } from 'core/types/Product';
 
-export type FilterForm = {
-    name?: string;
-    categoryId?: number;
-}
 type Props = {
-    onSearch: (filter: FilterForm) => void;
+    name?: string;
+    category?: Category;   
+    handleChangeName: (name: string) => void;
+    handleChangeCategory: (category: Category) => void;
+    clearFilters: () => void;
 }
 
-const ProductFilters = ({onSearch}:Props) => {
+const ProductFilters = ({
+    name,
+    category,
+    handleChangeCategory,
+    handleChangeName,
+    clearFilters
+
+}:Props) => {
     const [categories, setCategories] = useState<Category[]>([]);
     const [isLoadingCategories, setIsLoadingCategories] = useState(false);
-    const [name, setName] = useState('');
-    const [category, setCategory] = useState<Category>();
+
 
     useEffect(() => {
         setIsLoadingCategories(true)
@@ -28,20 +34,7 @@ const ProductFilters = ({onSearch}:Props) => {
             })
     }, []);
 
-    const handleChangeName= (name: string) =>{
-        setName(name);
-        onSearch({name, categoryId: category?.id});
-    }
-    const handleChangeCategory = (category: Category) => {
-        setCategory(category);
-        onSearch({name, categoryId: category?.id});
-    }
 
-    const clearFilters = ()=>{
-        setCategory(undefined);
-        setName('');
-        onSearch({name: '', categoryId: undefined})
-    }
     return (
         <div className='card-base product-filters-container'>
             <div className="input-search">
